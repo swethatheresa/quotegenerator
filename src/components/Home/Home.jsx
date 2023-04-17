5e11326d376fd5c15e2dd22867e9a50e995833be
swethatheresa <swethatheresa123@gmail.com>
2023-04-17 23:12:45 +0530

    added tags

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useEffect,useState } from 'react';
 import './Home.css'
+import { BsFillBookmarkPlusFill } from "react-icons/bs";
 
 function Home() {
 
@@ -13,12 +14,15 @@ function Home() {
     setLoading(true);
     fetch('https://api.quotable.io/tags')
       .then((response) => response.json())
-      .then((data) => setTags(data.results))
+      .then((data) => {
+        setTags(data)
+      })
       .catch((error) => console.error(error))
   }, []);
 
   useEffect(() => {
-    if (tags) {
+    if (tags && tags.length > 0) {
+      console.log(tags)
       setLoading(false);
     }
   }, [tags]);
@@ -45,6 +49,24 @@ function Home() {
     setSelectedTag(event.target.value);
   };
 
+  const handleNextQuote = () => {
+    if (selectedTag) {
+      setLoading(true);
+      fetch(`https://api.quotable.io/random?tags=${selectedTag}`)
+        .then((response) => response.json())
+        .then((data) => setQuote(data))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+    } else {
+      setLoading(true);
+      fetch('https://api.quotable.io/random')
+        .then((response) => response.json())
+        .then((data) => setQuote(data))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+    }
+  };
+  
   return (
     <div className="home">
       <div className="quotebody">
@@ -54,9 +76,11 @@ function Home() {
             <div>
               <div className="quote">{quote.content}</div>
               <div className="author">- {quote.author}</div>
+              <BsFillBookmarkPlusFill/>
             </div>
           )}
       </div>
+      
       <select className='tags 'value={selectedTag} onChange={handleTagChange}>
               <option value="">Random</option>
               {tags && tags.map((tag) => (
@@ -65,6 +89,7 @@ function Home() {
                 </option>
               ))}
       </select>
+      <button className='btn' onClick={handleNextQuote} >Next Quote</button>
     </div>
   );
 }
